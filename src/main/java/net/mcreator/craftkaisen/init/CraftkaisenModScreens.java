730a42a11478a5aea4c04a9ccb34942cd7333dee
RequiredDevelopment <pokem@192.168.1.221>
2023-07-07 18:13:47 -0400

    Menus added, no functionality yet

---
+++ src/main/java/net/mcreator/craftkaisen/init/CraftkaisenModScreens.java
@@ -0,0 +1,28 @@
+
+/*
+ *	MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.craftkaisen.init;
+
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.client.gui.screens.MenuScreens;
+
+import net.mcreator.craftkaisen.client.gui.SkillTreeMenuScreen;
+import net.mcreator.craftkaisen.client.gui.QuestMenuScreen;
+import net.mcreator.craftkaisen.client.gui.MainMenuScreen;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class CraftkaisenModScreens {
+	@SubscribeEvent
+	public static void clientLoad(FMLClientSetupEvent event) {
+		event.enqueueWork(() -> {
+			MenuScreens.register(CraftkaisenModMenus.SKILL_TREE_MENU.get(), SkillTreeMenuScreen::new);
+			MenuScreens.register(CraftkaisenModMenus.MAIN_MENU.get(), MainMenuScreen::new);
+			MenuScreens.register(CraftkaisenModMenus.QUEST_MENU.get(), QuestMenuScreen::new);
+		});
+	}
+}
