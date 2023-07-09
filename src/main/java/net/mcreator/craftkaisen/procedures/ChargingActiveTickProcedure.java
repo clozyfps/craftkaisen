0e2ac2962d4675e0ed4646f519f6a1e33511f0c0
clozyfps <jayso@10.0.0.21>
2023-07-09 15:20:08 -0400

    combat ready

--- src/main/java/net/mcreator/craftkaisen/procedures/ChargingActiveTickProcedure.java
+++ src/main/java/net/mcreator/craftkaisen/procedures/ChargingActiveTickProcedure.java
@@ -1,14 +1,21 @@
 package net.mcreator.craftkaisen.procedures;
 
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.Component;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
 
 import net.mcreator.craftkaisen.network.CraftkaisenModVariables;
 
 public class ChargingActiveTickProcedure {
-	public static void execute(Entity entity) {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
 		{
@@ -20,5 +27,11 @@ public class ChargingActiveTickProcedure {
 		}
 		if (entity instanceof LivingEntity _entity && !_entity.level.isClientSide())
 			_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 2, 3, true, false));
+		if (world instanceof ServerLevel _level)
+			_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+					"particle minecraft:dust 0 0 0 2 ^0 ^0 ^0 0.6 1.4 0.6 0 5");
+		if (world instanceof ServerLevel _level)
+			_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+					"particle minecraft:dust 0.12 0.84 0.73 2 ^0 ^0 ^0 0.6 1.4 0.6 0 5");
 	}
 }
--- src/main/java/net/mcreator/craftkaisen/procedures/DivineRepositeProcedureProcedure.java
+++ src/main/java/net/mcreator/craftkaisen/procedures/DivineRepositeProcedureProcedure.java
@@ -1,8 +1,14 @@
 package net.mcreator.craftkaisen.procedures;
 
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.network.chat.Component;
 
 import net.mcreator.craftkaisen.init.CraftkaisenModMobEffects;
 
@@ -10,7 +16,13 @@ public class DivineRepositeProcedureProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
-		if (entity instanceof LivingEntity _entity && !_entity.level.isClientSide())
-			_entity.addEffect(new MobEffectInstance(CraftkaisenModMobEffects.DIVINE_REPOSITE_POTION.get(), 3, 1, false, false));
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() instanceof SwordItem || (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() instanceof AxeItem
+				|| (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() instanceof PickaxeItem) {
+			if (entity instanceof LivingEntity _entity && !_entity.level.isClientSide())
+				_entity.addEffect(new MobEffectInstance(CraftkaisenModMobEffects.DIVINE_REPOSITE_POTION.get(), 3, 1, false, false));
+		} else {
+			if (entity instanceof Player _player && !_player.level.isClientSide())
+				_player.displayClientMessage(Component.literal("\u00A7l Must have a weapon!"), true);
+		}
 	}
 }
