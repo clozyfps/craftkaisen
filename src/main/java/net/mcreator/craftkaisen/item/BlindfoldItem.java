16463139b08b6b2d067b6b8321fdbbe0e0735f8b
opkatsu <rishi@MSI.myfiosgateway.com>
2023-07-19 19:34:15 -0400

    geto

--- src/main/java/net/mcreator/craftkaisen/item/BlindfoldItem.java
+++ src/main/java/net/mcreator/craftkaisen/item/BlindfoldItem.java
@@ -2,28 +2,18 @@
 package net.mcreator.craftkaisen.item;
 
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.Minecraft;
 
 import net.mcreator.craftkaisen.init.CraftkaisenModTabs;
-import net.mcreator.craftkaisen.client.model.Modelblindfold;
-
-import java.util.function.Consumer;
-import java.util.Map;
-import java.util.Collections;
 
 public abstract class BlindfoldItem extends ArmorItem {
 	public BlindfoldItem(EquipmentSlot slot, Item.Properties properties) {
@@ -73,24 +63,6 @@ public abstract class BlindfoldItem extends ArmorItem {
 	public static class Helmet extends BlindfoldItem {
 		public Helmet() {
 			super(EquipmentSlot.HEAD, new Item.Properties().tab(CraftkaisenModTabs.TAB_JJKARMOR));
-		}
-
-		@Override
-		public void initializeClient(Consumer<IClientItemExtensions> consumer) {
-			consumer.accept(new IClientItemExtensions() {
-				@Override
-				public HumanoidModel getHumanoidArmorModel(LivingEntity living, ItemStack stack, EquipmentSlot slot, HumanoidModel defaultModel) {
-					HumanoidModel armorModel = new HumanoidModel(new ModelPart(Collections.emptyList(),
-							Map.of("head", new Modelblindfold(Minecraft.getInstance().getEntityModels().bakeLayer(Modelblindfold.LAYER_LOCATION)).Head, "hat", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "body",
-									new ModelPart(Collections.emptyList(), Collections.emptyMap()), "right_arm", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "left_arm",
-									new ModelPart(Collections.emptyList(), Collections.emptyMap()), "right_leg", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "left_leg",
-									new ModelPart(Collections.emptyList(), Collections.emptyMap()))));
-					armorModel.crouching = living.isShiftKeyDown();
-					armorModel.riding = defaultModel.riding;
-					armorModel.young = living.isBaby();
-					return armorModel;
-				}
-			});
 		}
 
 		@Override
