6b2dd4e3b9f32c75ff492949dc4ca3bc539f7cd5
jayso <jayso@10.0.0.21>
2023-09-08 16:48:16 -0400

    MCreator Merge commit 'f8347f9a77f63ee9ce33973ea9c9a006067704d1' into 48e74366b8d3287f9799e07a9c17aabc540a7348

--- src/main/java/net/mcreator/craftkaisen/init/CraftkaisenModMenus.java
+++ src/main/java/net/mcreator/craftkaisen/init/CraftkaisenModMenus.java
@@ -13,7 +13,6 @@ import net.minecraft.world.inventory.MenuType;
 
 import net.mcreator.craftkaisen.world.inventory.SkillTreeMainGUIMenu;
 import net.mcreator.craftkaisen.world.inventory.MainMenuMenu;
-import net.mcreator.craftkaisen.world.inventory.InventoryCurseGUIMenu;
 import net.mcreator.craftkaisen.world.inventory.BindingVowGUIMenu;
 import net.mcreator.craftkaisen.CraftkaisenMod;
 
@@ -22,5 +21,4 @@ public class CraftkaisenModMenus {
 	public static final RegistryObject<MenuType<MainMenuMenu>> MAIN_MENU = REGISTRY.register("main_menu", () -> IForgeMenuType.create(MainMenuMenu::new));
 	public static final RegistryObject<MenuType<BindingVowGUIMenu>> BINDING_VOW_GUI = REGISTRY.register("binding_vow_gui", () -> IForgeMenuType.create(BindingVowGUIMenu::new));
 	public static final RegistryObject<MenuType<SkillTreeMainGUIMenu>> SKILL_TREE_MAIN_GUI = REGISTRY.register("skill_tree_main_gui", () -> IForgeMenuType.create(SkillTreeMainGUIMenu::new));
-	public static final RegistryObject<MenuType<InventoryCurseGUIMenu>> INVENTORY_CURSE_GUI = REGISTRY.register("inventory_curse_gui", () -> IForgeMenuType.create(InventoryCurseGUIMenu::new));
 }
