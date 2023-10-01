52fed6abb9e6c9cc8a382acfc3647e43cd513f43
jayso <jayso@10.0.0.21>
2023-10-01 17:36:22 -0400

    MCreator Merge commit '40f0b10c5707e1b948029cb2d9e0746b0b206763' into d7ff3b2196b72b4f512e363b629108dd80d1fc1a

--- src/main/java/net/mcreator/craftkaisen/init/CraftkaisenModSounds.java
+++ src/main/java/net/mcreator/craftkaisen/init/CraftkaisenModSounds.java
@@ -11,6 +11,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.resources.ResourceLocation;
 
+import net.mcreator.craftkaisen.CraftkaisenMod;
+
 public class CraftkaisenModSounds {
 	public static final DeferredRegister<SoundEvent> REGISTRY = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, CraftkaisenMod.MODID);
 	public static final RegistryObject<SoundEvent> BLOOD = REGISTRY.register("blood", () -> new SoundEvent(new ResourceLocation("craftkaisen", "blood")));
